01332e5ec54638c45ab1e926f726c83fdd617cba
matekerges <mate.kerges@gmail.com>
2022-12-13 17:56:49 +0100

    google auth with firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/Navigation/Navigation.component";
 
 import Home from "./routes/Home/Home.component";
+import SignIn from "./routes/SignIn/SignIn.component";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbGlzy464Ann03exPj0R33NANtGiA0-lY",
+  authDomain: "crwn-clothing-db-d6767.firebaseapp.com",
+  projectId: "crwn-clothing-db-d6767",
+  storageBucket: "crwn-clothing-db-d6767.appspot.com",
+  messagingSenderId: "601611789463",
+  appId: "1:601611789463:web:977cf09ae3c13f872975d2",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+};
