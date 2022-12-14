4f1bf266e4ce3aa6ad79a8119b2a2aa4fc89c3fe
matekerges <mate.kerges@gmail.com>
2022-12-14 19:41:30 +0100

    created sign up and button component

--- src/components/sign-up-form/SignUpForm.component.jsx
+++ src/components/sign-up-form/SignUpForm.component.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
+import FormInput from "../FormInput/FormInput.component";
+import Button from "../Button/Button.component";
 
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+
+import "./SignUpForm.style.css";
 
 const defaultFormFields = {
   displayName: "",
@@ -53,46 +57,43 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your email and password </h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
-        <label>Display Name</label>
-        <input
+        <FormInput
+          label="Display Name"
           type="text"
           required
           onChange={handleChange}
           name="displayName"
           value={displayName}
         />
-
-        <label>Email</label>
-        <input
+        <FormInput
+          label="Email"
           type="email"
           required
           onChange={handleChange}
           name="email"
           value={email}
         />
-
-        <label>Password</label>
-        <input
+        <FormInput
+          label="Password"
           type="password"
           required
           onChange={handleChange}
           name="password"
           value={password}
         />
-
-        <label>Confirm Password</label>
-        <input
+        <FormInput
+          label="Confirm Password"
           type="password"
           required
           onChange={handleChange}
           name="confirmPassword"
           value={confirmPassword}
         />
-
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
