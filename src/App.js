d33a3ce7bdaf6722a072c20bb005e716c40d73f1
matekerges <mate.kerges@gmail.com>
2022-12-15 14:48:02 +0100

    created auth page component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-import Navigation from "./routes/Navigation/Navigation.component";
 
 import Home from "./routes/Home/Home.component";
-import SignIn from "./routes/SignIn/SignIn.component";
+import Navigation from "./routes/Navigation/Navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -14,7 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,14 @@
+import SignUpForm from "../../components/sign-up-form/SignUpForm.component";
+import SignInForm from "../../components/sign-in-form/SignInForm.component";
+import "./authentication.styles.css";
+
+const Authentication = () => {
+  return (
+    <div className="authentication-container">
+      <SignInForm />
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Authentication;
