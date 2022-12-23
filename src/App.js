1fa97a6fa3bab17dffecae4a1631e69898d65f95
matekerges <mate.kerges@gmail.com>
2022-12-23 12:01:44 +0100

    created checkout page along with checkout items

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home/Home.component";
 import Navigation from "./routes/Navigation/Navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/Shop.component";
+import Shop from "./routes/Shop/Shop.component";
+import Checkout from "./routes/Checkout/Checkout.component";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
